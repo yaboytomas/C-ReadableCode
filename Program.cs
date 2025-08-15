894a427565b284651b70f568cda6b0becc4f2d5b
Tomas <tomasszabo94@gmail.com>
2025-08-15 18:59:01 -0400

    added proper whitespace and indents and also commented it

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+    this code reverses a message, counts the number of times a 
+    char appears and then prints the results.
+*/
+string str = "The quick brown fox jumps over the lazy dog.";
+
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
+
+int x = 0;
+
+foreach (char i in charMessage) 
+{
+    if (i == 'o')
+    {
+        x++;
+    }
+}
+
+string new_message = new String(charMessage); 
+
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
